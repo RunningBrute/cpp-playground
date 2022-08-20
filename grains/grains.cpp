7db9b90bd87b5b1c6ea02ede5faf1ac3826ba985
RunningBrute <piotrekbas8@gmail.com>
2022-08-20 18:53:39 +0200

    Complete excercise (grains) - fix last 2 uts

--- grains/grains.cpp
+++ grains/grains.cpp
@@ -5,17 +5,22 @@ namespace grains
 
 unsigned long long square(int number)
 {
-    int result=1;
+    unsigned long long result=1;
+
     for (int i = 1; i < number; i++)
-    {
         result = result * 2;
-    }
+
     return result;
 }
 
 unsigned long long total()
 {
-    return square(64);
+    unsigned long long total = 0;
+
+    for (int i=1; i<=64; i++)
+        total = total + square(i);
+
+    return total;
 }
 
 }
