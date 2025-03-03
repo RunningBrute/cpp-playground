32e0712311a75ce1f18a5c3a55de668ff2e59a2a
Piotr Basiński <piotrekbas8@gmail.com>
2025-03-03 22:04:06 +0000

    Add first example file

---
+++ 01_Basics/basics.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Car
+{
+public:
+    std::string brand;
+    int year;
+
+    void display() 
+    {
+      std::cout << "Car brand: " << brand
+                << ", Year: " << year
+                << std::endl;
+    }
+};
+
+int main()
+{
+  Car myCar;
+  myCar.brand = "Toyota";
+  myCar.year = 2022;
+  myCar.display();
+
+  return 0;
+}
